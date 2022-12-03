581db09683585fa1a0842a1d99665a8177ef0fb7
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2022-12-03 13:59:03 -0300

    add tailwind

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,75 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import  { useState } from 'react';
-import { FiSearch } from 'react-icons/fi';
-import './styles.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
 
-import api from './services/api';
+import api from "./services/api";
 
-function App() 
-{
+function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
 
-  const[input, setInput] = useState('')
-  const[cep, setCep] = useState({});
-
-  async function handleSearch()
-  {
+  async function handleSearch() {
     // 01310930/json
 
-    if(input === '')
-    {
-      alert("Informe seu CEP!")
+    if (input === "") {
+      alert("Informe seu CEP!");
       return;
     }
 
-    try
-    {
-      const response = await api.get(input + '/json');
+    try {
+      const response = await api.get(input + "/json");
       setCep(response.data);
       setInput("");
-    }
-    catch
-    {
-      alert("Ops! Ocorreu um erro ao buscar o CEP.")
-      setInput("")
+    } catch {
+      alert("Ops! Ocorreu um erro ao buscar o CEP.");
+      setInput("");
     }
   }
 
   return (
-    <div className='container'>
-      <h1 className='tittle'>Cep Finder</h1>
-      <div className='containerInput'>
-        <input
-          type='text'
-          placeholder='Digite seu cep. . .'
+    <div className="mt-32">
+      <div className="w-full h-full flex justify-center">
+        <div>
+          <h1 className="tittle text-center text-lg my-10">Cep Finder</h1>
+          <div className="containerInput flex row align-middle">
+            <input
+              className="bg-gray-300 rounded-md p-1 mx-2"
+              type="text"
+              placeholder="Digite seu cep. . ."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
 
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-
-        <button 
-          className='buttonSearch'
-          onClick={handleSearch}
-        >
-          <FiSearch size={20} color='white'/>
-        </button>
+            <button className="buttonSearch" onClick={handleSearch}>
+              <FiSearch size={20} color="black" />
+            </button>
+          </div>
+        </div>
       </div>
-      
-
-      {
-        Object.keys(cep).length > 0 && 
-        (
-          <main className='main'>
+      <div className="w-full text-sm">
+        {Object.keys(cep).length > 0 && (
+          <main className="main text-center mt-10">
             <h2>CEP: {cep.cep}</h2>
+            <br />
             <span>Rua: {cep.logradouro}</span>
+            <br />
             <span>Complemento: {cep.complemento}</span>
+            <br />
             <span>Bairro: {cep.bairro}</span>
+            <br />
             <span>Cidade: {cep.localidade}</span>
+            <br />
             <span>Estado / UF: {cep.uf}</span>
+            <br />
           </main>
-        )
-      }
+        )}
+      </div>
     </div>
   );
 }
